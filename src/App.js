a035ea44ba22e5754788d5c0bf9c0db1f33b48bd
JaiKumar1003 <156499686+JaiKumar1003@users.noreply.github.com>
2024-03-19 18:54:16 +0530

    Output Error

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Home from './components/Home'
 import About from './components/About'
 import NotFound from './components/NotFound'
 
+import './App.css'
+
 const App = () => (
-  <div>
+  <div className="app-container">
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
