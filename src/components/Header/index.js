ce94e35ce7df00abcad8b1f7b5b2a22c5f5eab09
JaiKumar1003 <156499686+JaiKumar1003@users.noreply.github.com>
2024-03-19 18:50:08 +0530

    Output Error

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,48 @@
 // Write your code here
 
+import Popup from 'reactjs-popup'
 import {GiHamburgerMenu} from 'react-icons/gi'
+import {IoMdClose} from 'react-icons/io'
 
 import './index.css'
 
-const Header = () => {
+const Header = props => {
+  const {isClickedMenu, onClickMenu} = props
+  console.log(isClickedMenu)
+  const onClickHarmburger = () => {
+    onClickMenu()
+  }
   return (
     <div className="header-card">
       <img
         className="header-logo"
         src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
-        alt="website-logo"
+        alt="website logo"
       />
-      <GiHamburgerMenu className="header-menu" />
+
+      <div>
+        <Popup
+          model
+          trigger={
+            <button
+              onClick={onClickHarmburger}
+              type="button"
+              className="hamburger-btn"
+            >
+              {isClickedMenu ? (
+                <IoMdClose className="header-hamburger-icon" />
+              ) : (
+                <GiHamburgerMenu className="header-hamburger-icon" />
+              )}
+            </button>
+          }
+          className="popup-content"
+        >
+          <div>
+            <p>Hai Jai</p>
+          </div>
+        </Popup>
+      </div>
     </div>
   )
 }
