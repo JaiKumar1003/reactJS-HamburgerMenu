2d292887c913abfa13ada25c3343b985b5ed7829
JaiKumar1003 <156499686+JaiKumar1003@users.noreply.github.com>
2024-03-19 18:52:15 +0530

    Output Error

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,16 +1,27 @@
 // Write your code here
 
-import {Link} from 'react-router-dom'
+import Header from '../Header'
 
 import './index.css'
 
 const NotFound = () => {
   return (
-    <div>
-      <Link to="/not-found">
-        <p>Hello</p>
-      </Link>
-    </div>
+    <>
+      <Header />
+      <hr className="line" />
+      <div className="not-found-card">
+        <img
+          className="not-found-img"
+          src="https://assets.ccbp.in/frontend/react-js/not-found-img.png"
+          alt="not found"
+        />
+        <h1 className="not-found-heading">Lost Your Way?</h1>
+        <p className="not-found-description">
+          Sorry we cannot find that page. We will find lots to explore on the
+          home page
+        </p>
+      </div>
+    </>
   )
 }
 
