719228b887b984e2ea58864fc0d8bcd58d3eedb6
JaiKumar1003 <156499686+JaiKumar1003@users.noreply.github.com>
2024-03-19 18:47:33 +0530

    Output Error

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,22 @@
 // Write your code here
 
+import Header from '../Header'
+
 import './index.css'
 
 const About = () => {
   return (
-    <div>
-      <p>Hai</p>
-    </div>
+    <>
+      <Header />
+      <hr className="line" />
+      <div className="about-card">
+        <img
+          className="about-img"
+          src="https://assets.ccbp.in/frontend/react-js/about-sm-img.png"
+          alt="about"
+        />
+      </div>
+    </>
   )
 }
 
