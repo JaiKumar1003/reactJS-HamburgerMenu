8d296cea78130abcc7e9958b998d722b90830bec
JaiKumar1003 <156499686+JaiKumar1003@users.noreply.github.com>
2024-03-19 18:51:11 +0530

    Output Error

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,11 +7,31 @@ import Header from '../Header'
 import './index.css'
 
 class Home extends Component {
+  state = {isClickedMenu: false}
+
+  onClickMenu = () => {
+    this.setState({isClickedMenu: true})
+  }
+
+  renderPopup = () => <p>jai</p>
+
   render() {
+    const {isClickedMenu} = this.state
     return (
-      <div>
-        <Header />
-        <p>Jai</p>
+      <div className="home-container">
+        <Header isClickedMenu={isClickedMenu} onClickMenu={this.onClickMenu} />
+        <hr className="line" />
+        <div className="home-card">
+          {isClickedMenu ? (
+            this.renderPopup()
+          ) : (
+            <img
+              className="home-img"
+              src="https://assets.ccbp.in/frontend/react-js/home-sm-img.png"
+              alt="home"
+            />
+          )}
+        </div>
       </div>
     )
   }
